590447af1f0060cada1f1aaca508308d846d58a0
Jonathan Ortigosa-Hernández <jonathan.ortigosa@gmail.com>
2016-07-31 18:26:27 +0200

    Added files: class to calculate the Imbalance Ratio, another to calculate the Imbalance Degree and a test file for both classes

---
+++ test.py
@@ -0,0 +1,34 @@
+import numpy as np
+from imbalancedegree import ImbalanceDegree
+from imbalanceratio import ImbalanceRatio
+
+#Variables
+complementMeasures = ['MANHATTAN_DISTANCE','EUCLIDEAN_DISTANCE','CHEBYSHEV_DISTANCE','KULLBACKLEIBLER_DIVERGENCE','HELLIGER_DISTANCE','TOTALVARIATION_DISTANCE','CHISQUARE_DIVERGENCE']
+
+
+#Random generation of a class distribution by sampling a Dirichlet
+classDistribution = np.random.dirichlet(np.random.randint(50,size= np.random.randint(2,10,size=1)), 1)
+#Flatten the output list
+classDistribution = [item for sublist in classDistribution for item in sublist]
+
+print '============================='
+print 'SAMPLED CLASS DISTRIBUTION'
+print '============================='
+stats = ImbalanceDegree(ocurrences)
+print 'Number of classes: ' + str(stats.numClasses)
+print 'Number of minority classes: ' + str(stats.numMinorityClasses)
+print 'Class Distribution: ' + str([ '%.2f' % elem for elem in stats.classDistribution])
+print 'Balanced distribution: ' + str([ '%.2f' % elem for elem in stats.balancedClassDistribution])
+print 'Distribution with the lowest entropy: ' + str([ '%.2f' % elem for elem in stats.lowestEntropyClassDistribution])
+
+print '============================='
+print 'MEASURES'
+print '============================='
+#IMBALANCE RATIO
+IR = ImbalanceRatio(ocurrences)
+print 'IMBALANCE_RATIO: ' + str("{0:.2f}".format(IR.value))
+print '-'
+#IMBALANCE DEGREE
+for complementMeasure in complementMeasures:
+	ID = ImbalanceDegree(ocurrences,complementMeasure)
+	print 'IMBALANCE_DEGREE_'+str(complementMeasure) + ': ' + str("{0:.2f}".format(ID.value))
